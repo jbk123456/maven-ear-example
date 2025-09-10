2b4146038375d7797d06a6974d82b67ec08b99fc
Jost Boekemeier <jbk123456@github.com.com>
2025-09-10 06:13:57 +0200

    added MemoryConfigSource from https://github.com/microprofile-extensions/config-ext/blob/main/README.md

---
+++ module-web/src/main/java/com/example/rest/ApplicationConfig.java
@@ -0,0 +1,24 @@
+package com.example.rest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Activate JAX-RS. 
+ * All REST Endpoints available under /api
+ * 
+ * @author <a href="mailto:phillip.kruger@phillip-kruger.com">Phillip Kruger</a>
+ */
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<Class<?>>();
+        classes.add(MemoryConfigApi.class);
+        return classes;
+    }
+}
---
+++ module-web/src/main/java/com/example/rest/ConfigSourceMapProvider.java
@@ -0,0 +1,38 @@
+package com.example.rest;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Provider;
+
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
+/**
+ * Making the Config sources available via CDI
+ * @author <a href="mailto:phillip.kruger@phillip-kruger.com">Phillip Kruger</a>
+ */
+@ApplicationScoped
+public class ConfigSourceMapProvider {
+    
+    private static final Logger log = Logger.getLogger(ConfigSourceMapProvider.class.getName());
+    
+    @Inject
+    private Provider<Config> configProvider;
+    
+    private final Map<String,ConfigSource> configSourceMap = new HashMap<>();
+    
+    @Produces @ConfigSourceMap
+    public Map<String,ConfigSource> produceConfigSourceMap(){
+        if(this.configSourceMap.isEmpty()){
+            for(ConfigSource configSource:configProvider.get().getConfigSources()){
+                this.configSourceMap.put(configSource.getName(), configSource);
+            }
+        }
+        return this.configSourceMap;
+    }
+}
