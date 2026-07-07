fb259ccaf607052e339a5e7f55c5c94d080448b7
Jost Boekemeier <jbk123456@github.com>
2026-07-07 09:15:27 +0200

    .

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+c=/c/dev/daisy
+export PATH=/usr/bin:/bin:$c/git:$c/git/bin:$c/java/jdk1.8.0_202/bin:$c/apache-maven-3.9.16/bin
+export JAVA_HOME=$c/java/jdk1.8.0_202
+export HOME=$c
+
---
+++ start.sh
@@ -0,0 +1,1 @@
+mvn -Dmaven.repo.local=/c/dev/daisy/maven-repos/ -s c:/dev/daisy/maven-settings/settings.xml -gs c:/dev/daisy/maven-settings/settings.xml io.openliberty.tools:liberty-maven-plugin:3.11.3:run
